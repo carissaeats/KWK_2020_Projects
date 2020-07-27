a1ae060dbb1c001a5e307c16b627b451087482e4
Carissa Liu <carissaliu@rocketmail.com>
2020-07-27 10:35:35 -0700

    added Intro_to_Segues

---
+++ Intro_to_Segues/Intro_to_Segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intro_to_Segues
+//
+//  Created by Apple on 7/23/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
