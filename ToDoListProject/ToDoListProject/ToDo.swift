a1a9a07105e1e04e98ec3a663fea464148f94ba2
Carissa Liu <carissaliu@rocketmail.com>
2020-07-27 18:17:19 -0700

    Completed iteration 0

---
+++ ToDoListProject/ToDoListProject/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListProject
+//
+//  Created by Apple on 7/27/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+    var description = ""
+    var important = false
+}
